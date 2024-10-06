c756152081ef4ded5ea6dc15f6d266ca40c14968
Jonatas Fischer <jonatas.fischer@hotmail.com>
2024-10-06 06:15:48 +0200

    Inproving key positons

--- src/main/java/com/keyboard/generator/keyboard/mx/CherryMXSwitchPlate.java
+++ src/main/java/com/keyboard/generator/keyboard/mx/CherryMXSwitchPlate.java
@@ -7,9 +7,7 @@ import eu.mihosoft.vvecmath.Vector3d;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.keyboard.generator.keyboard.mx.CherryMXPlateParameters.*;
 
